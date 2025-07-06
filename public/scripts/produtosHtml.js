6114418e62f45aa121a544e0b1b7698771228f04
IsadoraCampos <sbeghenisadora@gmail.com>
2025-07-06 10:25:58 -0300

    create e delete de produtos

--- public/scripts/produtosHtml.js
+++ public/scripts/produtosHtml.js
@@ -4,6 +4,7 @@ axios.defaults.headers.common["Content-Type"] =
 
 document.addEventListener("DOMContentLoaded", () => {
     tabelaProdutos();
+    novoProduto();
 });
 
 function tabelaProdutos() {
@@ -17,6 +18,10 @@ function tabelaProdutos() {
 
 function processaResultadoProduto(rows) {
     const tabelaProdutos = document.getElementById('tabelaProdutos');
+    if (!tabelaProdutos) {
+        return;
+    }
+
     tabelaProdutos.innerHTML = '';
 
     let tabelaResultado = `
@@ -35,16 +40,62 @@ function processaResultadoProduto(rows) {
     for (let i = 0; i < rows.length; i++) {
         tabelaResultado += `<tr> <td class="linha-tabela">${rows[i].nome}</td>`;
         tabelaResultado += `<td class="linha-tabela">${rows[i].descr}</td>`;
-        tabelaResultado += `<td class="linha-tabela">${rows[i].valor}</td>`;
+        tabelaResultado += `<td class="linha-tabela">R$ ${rows[i].valor.replace('.', ',')}</td>`;
         tabelaResultado += `<td class="linha-tabela">${rows[i].quant_produzida}</td>`;
         tabelaResultado += `<td class="linha-tabela">${formataData(rows[i].data_validade)}</td>`;
         tabelaResultado += `<td>
                                 <div class="is-flex is-gap-3">
                                     <a href="#" class="btn btn-info btn-table" onclick="openForm('editar-produto')">Editar</a>
-                                    <a href="#" class="btn btn-danger btn-table">Deletar</a>
+                                    <a href="#" class="btn btn-danger btn-table" onclick="deletarProduto('${rows[i].nome}', '${rows[i].descr}')">Deletar</a>
                                 </div>
                             </td> </tr>`;                  
     }
 
     tabelaProdutos.innerHTML = tabelaResultado;
+}
+
+function novoProduto() {
+    const formProduto = document.getElementById('produtos-form');
+    if (!formProduto) {
+        return;
+    }
+
+    formProduto.addEventListener('submit', async (event) => {
+        event.preventDefault();
+
+        const nome = document.getElementById('nome-produto').value;
+        const descr = document.getElementById('descr-produto').value;
+        const valorString = document.getElementById('valor-produto').value.replace('R$', '').replace(',', '.');
+        const valor = parseFloat(valorString);
+        const quant = document.getElementById('quant-produto').value;
+        const data_validade = document.getElementById('data-produto').value;
+
+        try {
+            await axios.post('/api/produto', {nome, descr, valor, quant, data_validade});
+            console.log('Produto cadastrado com sucesso!');
+            formProduto.reset();
+            window.location.href = 'http://localhost:3000/produtos';
+        } catch (error) {
+            console.log('Ocorreu um erro ao inserir um produto: ', error);
+        }
+    });
+}
+
+function editarProduto() {
+    const editarForm = document.getElementById('produtos-editar');
+    if (!editarForm) {
+        return;
+    }
+
+    
+}
+
+async function deletarProduto(nome, descr) {
+    try {
+        await axios.delete('/api/produto', {params: {nome, descr}});
+        console.log('Produto deletado com sucesso!');
+        window.location.reload();
+    } catch (error) {
+        console.log('Ocorreu um erro ao deletar um produto: ', error);
+    }
 }
--- server.js
+++ server.js
@@ -52,3 +52,57 @@ app.get("/api/produto", async (req, res) => {
   }
 });
 
+app.post("/api/produto", async (req, res) => {
+  try {
+    const nome = req.body.nome;
+    const descr = req.body.descr;
+    const data_validade = req.body.data_validade;
+    const valor = req.body.valor;
+    const quant = req.body.quant;
+
+    const novoProduto = await db.one(
+      "INSERT INTO produto(nome,descr,valor,quant_produzida,data_validade) VALUES ($1, $2, $3, $4, $5) RETURNING *;",
+      [nome, descr, valor, quant, data_validade]
+    );
+
+    console.log(`Produto ${novoProduto.descr} criado!`);
+    res.sendStatus(200).json(novoProduto);
+  } catch(error) {
+    console.log(error);
+    res.sendStatus(400).json({error: error.message});
+  }
+});
+
+app.put("/api/produto", async (req, res) => {
+  try {
+    const nome = req.body.nome;
+    const descr = req.body.descr;
+    const data_validade = req.body.data_validade;
+    const valor = req.body.valor;
+    const quant = req.body.quant;
+
+    await db.none("UPDATE produto SET nome = $1, descr = $2, valor = $3, quant_produzida = $4, data_validade = $5 WHERE nome = $1 AND descr = $2;",
+      [nome, descr, valor, quant, data_validade]
+    );
+
+    console.log("Produto alterado com sucesso");
+    res.sendStatus(200);
+  } catch (error) {
+    console.log(error);
+    res.sendStatus(400).json({error: error.message});
+  }
+});
+
+app.delete("/api/produto", async (req, res) => {
+  try {
+    const nome = req.query.nome;
+    const descr = req.query.descr;
+
+    await db.none("DELETE FROM produto WHERE nome = $1 AND descr = $2;", [nome, descr]);
+    console.log("Produto removido");
+    res.sendStatus(200);
+  } catch (error) {
+    console.log(error);
+    res.sendStatus(400).json({error: error.message});
+  }
+});
